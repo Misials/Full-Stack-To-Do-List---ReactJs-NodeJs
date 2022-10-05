56ccd9fff724beed4b6fd1e8fa0bb4694a9f30c8
Misials <michalski.mikolaj10@wp.pl>
2022-10-05 07:20:47 +0200

    Backend - Connected to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const DB = process.env.DATABASE_URI.replace('<password>', process.env.DATABASE_PASSWORD);
 
 app.get('/', (req, res) => {
 	res.send('Backend Side');
 });
 
+mongoose
+	.connect(DB, {
+		useNewUrlParser: true,
+	})
+	.then(() => {
+		console.log(`Connected to MongoDB`);
+	});
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
